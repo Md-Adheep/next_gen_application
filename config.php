eadfe196e91985ac41f00a0374511f31423e8cf5
Md-Adheep <mohamedadeep59@gmail.com>
2026-03-19 11:50:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // ── Database ─────────────────────────────────────────────────
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'db');
 define('DB_PORT', 3306);
 define('DB_NAME', 'nextgen_db');
 
